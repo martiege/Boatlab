f81fbcff6edd9e77d1c29e5d472d5f171edf93e8
Unknown <martiege@stud.ntnu.no>
2018-11-24 10:45:22 +0100

    some plotting happened

--- boatlab/problem 5.5/p5p5d.m
+++ boatlab/problem 5.5/p5p5d.m
@@ -1,15 +1,24 @@
+info = open("p5p5d.mat");
+info = info.ans;
+
+time         = info(1, :);
+compass_meas = info(2, :);
+compass_est  = info(3, :);
+rudder       = info(4, :);
+bias_est     = info(5, :);
+
 compass = open("p5p5d_com.mat");
 compass = compass.ans;
 
-rudder = open("p5p5d_rudder.mat");
-rudder = rudder.ans;
-
-bias = open("p5p5d_bias.mat");
-bias = bias.ans;
-
-time = compass(1, :);
-compass_meas = compass(2, :);
-compass_est  = compass(3, :);
+% rudder = open("p5p5d_rudder.mat");
+% rudder = rudder.ans;
+% 
+% bias = open("p5p5d_bias.mat");
+% bias = bias.ans;
+% 
+% time = compass(1, :);
+% compass_meas = compass(2, :);
+% compass_est  = compass(3, :);
 
 figure(1)
 plot(time, compass_meas);
@@ -18,17 +27,12 @@ xlabel("Time [s]");
 ylabel("Compass [°]");
 title("Measured compass with estimated bias removed");
 
-time = bias(1, :);
-bias_est = bias(2, :);
-
-
-
 figure(2);
-plot(time, bias_est);
-legend("Estimated bias");
+plot(time, bias_est, time, rudder, time, (rudder - bias_est));
+legend("Estimated bias", "Rudder input", "Rudder without bias removed");
 xlabel("Time [s]");
-ylabel("Compass [°]");
-title("Bias estimation from Kalman filter");
+ylabel("Rudder info [°]");
+title("Rudder input and bias estimation");
 
 
 
